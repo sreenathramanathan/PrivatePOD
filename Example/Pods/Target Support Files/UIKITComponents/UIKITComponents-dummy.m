441bef1790c1785f90b5d484a9c72d43ecfeb3a6
sreenath <sreenath.ramanathan@nike.com>
2016-07-12 10:00:20 -0700

    pod spec changes

---
+++ Example/Pods/Target Support Files/UIKITComponents/UIKITComponents-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIKITComponents : NSObject
+@end
+@implementation PodsDummy_UIKITComponents
+@end
---
+++ Example/Pods/Target Support Files/UIKITComponents/UIKITComponents-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double UIKITComponentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIKITComponentsVersionString[];
+
